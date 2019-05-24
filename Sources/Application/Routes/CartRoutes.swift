80f1274ae6b8df1386c82db8b5fd3c2176f83c50
David Dunn <davdunn2@uk.ibm.com>
2019-05-24 16:17:19 +0100

    Add raw session for shopping cart

--- Sources/Application/Routes/CartRoutes.swift
+++ Sources/Application/Routes/CartRoutes.swift
@@ -1,8 +1,37 @@
-//
-//  CartRoutes.swift
-//  Application
-//
-//  Created by David Dunn on 24/05/2019.
-//
+import KituraSession
 
-import Foundation
+func initializeCartRoutes(app: App) {
+    
+    let session = Session(secret: "password", cookie: [CookieParameter.name("cart-cookie")])
+    
+    app.router.all(middleware: session)
+    app.router.get("/cart") { request, response, next in
+        guard let session = request.session else {
+            return try response.status(.internalServerError).end()
+        }
+        
+        let books: [Book] = session["books"] ?? []
+        response.send(books)
+        next()
+    }
+    
+    app.router.post("/cart") { request, response, next in
+        guard let session = request.session else {
+            return try response.status(.internalServerError).end()
+        }
+        
+        guard let book = try? request.read(as: Book.self) else {
+            let _ = response.send(status: .badRequest)
+            return next()
+        }
+        
+        var books: [Book] = session["books"] ?? []
+        
+        books.append(book)
+        session["books"] = books
+        
+        response.status(.created)
+        response.send(book)
+        next()
+    }
+}
