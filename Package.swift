e0b26a3eeb391d58ead4390524febf20def4405f
David Dunn <davdunn2@uk.ibm.com>
2019-05-24 15:09:46 +0100

    Remove Package.resolved

--- Package.swift
+++ Package.swift
@@ -13,10 +13,11 @@ let package = Package(
 	  .package(url: "https://github.com/IBM-Swift/Swift-Kuery-PostgreSQL.git", from: "2.1.1"),
 	  .package(url: "https://github.com/IBM-Swift/Swift-Kuery-ORM.git", from: "0.5.2"),
       .package(url: "https://github.com/IBM-Swift/Kitura-CredentialsHTTP.git", from: "2.1.3"),
+      .package(url: "https://github.com/IBM-Swift/Kitura-Session", from: "3.3.3"),
 	],
     targets: [
       .target(name: "BookBackend", dependencies: [ .target(name: "Application"), "Kitura" , "HeliumLogger"]),
-      .target(name: "Application", dependencies: [ "Kitura", "CloudEnvironment","SwiftMetrics", "Health", "KituraOpenAPI", "SwiftKueryORM", "SwiftKueryPostgreSQL", "CredentialsHTTP"]),
+      .target(name: "Application", dependencies: [ "Kitura", "CloudEnvironment","SwiftMetrics", "Health", "KituraOpenAPI", "SwiftKueryORM", "SwiftKueryPostgreSQL", "CredentialsHTTP", "KituraSession"]),
 
       .testTarget(name: "ApplicationTests" , dependencies: [.target(name: "Application"), "Kitura","HeliumLogger" ])
     ]
